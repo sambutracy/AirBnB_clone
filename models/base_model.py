df9d4ec27f6703e64ecd6a38877da5d8386fd25a
vintage254 <derricknjuguna414@gmail.com>
2024-03-08 05:35:24 -0800

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,26 @@ from datetime import datetime
 class BaseModel that defines all common attributes/methods for other classes
 """
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
+        if kwargs is not empty - each key of this dictionary is an attribute name (Note __class__ from kwargs is the only one that should not be added as an attribute.
         id: string - assign with an uuid when an instance is created
         created_at: datetime - assign with the current datetime when an instance is created
         updated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
+        t_format =  "%Y-%m-&dT&H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.item():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or  "updated_at":
+                    setattr(self, key, datetime.strptime(value, t_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
     def save(self):
 
         """
